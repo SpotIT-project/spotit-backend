54a881e964fc5c932cf6189efc2c48b90c515ef8
SavaS1004 <savaalex1004@yahoo.com>
2026-04-29 14:21:46 +0300

    Add migration for PhotoUrl column on posts table

---
+++ src/SpotIt.Infrastructure/Data/Migrations/20260429112115_AddPhotoUrlToPost.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotIt.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoUrlToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "photo_url",
+                table: "posts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "photo_url",
+                table: "posts");
+        }
+    }
+}
